adc99b0a20548ee9b9c8699f4660de77a58352a3
martinllobell <martinllobell.3.0@gmail.com>
2024-11-18 10:07:42 -0300

    dist addition

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ export default defineConfig({
   output: 'static', // Asegúrate de que se genera un sitio estático
   base: '/lionware/', // Ruta base del repositorio (nombre del repositorio)
   site: 'https://lionware-solutions.github.io/lionware/', // URL pública de tu sitio en GitHub Pages
-  outDir: './docs',
+  outDir: './dist',
 });
