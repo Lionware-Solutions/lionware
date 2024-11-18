15fc29f523f93b70deafbbebb479cc800cf478ad
martinllobell <martinllobell.3.0@gmail.com>
2024-11-18 09:53:42 -0300

    deploy config v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,7 @@ import vue from '@astrojs/vue';
 export default defineConfig({
   integrations: [tailwind(), icon(), vue()],
   output: 'static', // Asegúrate de que se genera un sitio estático
-  base: '/lionware/', // Ruta base del repositorio (nombre del repositorio)
+  // base: '/lionware/', // Ruta base del repositorio (nombre del repositorio)
   site: 'https://lionware-solutions.github.io/lionware/', // URL pública de tu sitio en GitHub Pages
   outDir: './docs',
-  build: {
-    assetsPrefix: '/lionware/',
-  },
 });
